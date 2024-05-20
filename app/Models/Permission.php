b921e02d7e3423f5d990825eebe1f30129b0ad6d
duows <henriquesouzaoficial004@gmail.com>
2024-05-20 19:48:43 -0300

    Permission model implemented!

---
+++ app/Models/Permission.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    use HasFactory;
+
+    protected $table = "permission";
+
+    protected $fillable = [
+        "role"];
+
+  
+    #definir regras para esse model
+    #user rules
+    public function rules()
+    {
+        return [
+            'role' => 'required',
+        ];
+    }
+
+    public function feedback()
+    {
+        return [
+            'role' => 'O campo :attribute é obrigatório',
+        ];
+    }
+}
